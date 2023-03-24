7faafd5d9bccd698e34a6f6e16cca1c0ea087b8c
ORIKEMARY96 <chukwuebukamary2@gmail.com>
2023-03-23 21:41:33 -0700

    0x0f function pointers task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%.2hx", a[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: O
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hx\n", arr[i]);
+			break;
+		}
+		printf("%02hx ", arr[i]);
+	}
+	return (0);
+}
