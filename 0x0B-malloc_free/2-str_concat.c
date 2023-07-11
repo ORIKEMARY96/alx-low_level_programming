71e356d3bf500fc8045231f0d7ddefec2b584f43
ORIKEMARY96 <chukwuebukamary2@gmail.com>
2023-07-11 15:37:07 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,33 +11,42 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
-	unsigned int i, j, k, limit;
+	unsigned int i, j, len1, len2;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	len1 = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
+	len2 = 0;
 
-	for (j = 0; s2[j] != '\0'; j++)
-		;
+	while (s1 && s1[len1] != '\0')
+		len1++;
+	while (s2 && s2[len2] != '\0')
+		len2++;
 
-	strout = malloc(sizeof(char) * (i + j + 1));
-
-	if (strout == NULL)
-	{
-		free(strout);
+	strout = malloc(sizeof(char) * (len1 + len2 + 1));
+        if (strout == NULL)
 		return (NULL);
+	i = 0;
+	j = 0;
+
+        if (s1 == NULL)
+	{
+		return (NULL);
+
+	   for (i = 0; i < len1; i++)
+	   {
+               strout[i] = s1[i];
+	   }
+
 	}
+	 if (s2 == NULL)
+	 {
 
-	for (k = 0; k < i; k++)
-		strout[k] = s1[k];
-
-	limit = j;
-	for (j = 0; j <= limit; k++, j++)
-		strout[k] = s2[j];
-
-	return (strout);
+	 for (j = 0; j < len1 + len2; j++)
+	 {
+            strout[j] = s2[j];
+	 }
+	 return (NULL);
+	 }
+	 strout[i] = '\0';
+	 return (strout);
 }
