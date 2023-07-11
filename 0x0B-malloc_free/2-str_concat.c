5f6a5f91270ae23016a0e67dc7080f9ebeb5e222
ORIKEMARY96 <chukwuebukamary2@gmail.com>
2023-07-11 16:25:32 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- *
- * Return: pointer of an array of chars
+ *  *str_concat - concatenate two strings
+ *  @s1: string to concatenate
+ *  @s2: other string to concatenate
+ *  Return: pointer to the new string or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -14,39 +13,43 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, len1, len2;
 
 	len1 = 0;
-
 	len2 = 0;
 
 	while (s1 && s1[len1] != '\0')
 		len1++;
+
 	while (s2 && s2[len2] != '\0')
 		len2++;
-
 	strout = malloc(sizeof(char) * (len1 + len2 + 1));
-        if (strout == NULL)
-		return (NULL);
-	i = 0;
-	j = 0;
 
-        if (s1 == NULL)
+	if (strout == NULL)
+		return (NULL);
+
+	if (s1 == NULL)
 	{
-		return (NULL);
 
-	   for (i = 0; i < len1; i++)
-	   {
-               strout[i] = s1[i];
-	   }
 
+	for (i = 0; i < len1; i++)
+	{
+
+	  strout[i] = s1[i];
 	}
-	 if (s2 == NULL)
-	 {
 
-	 for (j = 0; j < len1 + len2; j++)
-	 {
-            strout[j] = s2[j];
-	 }
-	 return (NULL);
-	 }
-	 strout[i] = '\0';
-	 return (strout);
+
+           return (NULL);
+	}
+        
+        if (s2 == NULL)
+	{
+	for (j = 0; j < len1 + len2; j++)
+	{
+	  strout[j] = s2[j];
+	}
+
+          return (NULL);
+	}
+	
+	return (strout);
+
 }
+
