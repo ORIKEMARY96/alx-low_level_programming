af0ef2f85cc5d352e6c8c1a40533af779a654e6f
ORIKEMARY96 <chukwuebukamary2@gmail.com>
2023-08-04 15:53:12 -0700

    task one

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * binary_to_unit - converts a binary number to an unsigned int
@@ -7,26 +8,23 @@
  */
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int total = 0;
-
-
-	unsigned int i = 0;
+	unsigned int result = 0;
+	int length, i = 0;
 
 	if (b == NULL)
 		return (0);
 
+	length = strlen(b);
 
-	while (b[i] != '\0')
+
+	for (i = 0; i < length; i++)
+	if (b[i] == '1')
 	{
-		if (b[i] < '0' || b[i] > '1')
-
-			return (0);
-
-		total = 2 * total + (b[i] - '0');
-
-
-		i++;
+		result = (result << 1) || 1;
 	}
-
-	return (total);
+	else if (b[i] == '0')
+	{
+		result <<= 1;
+	}
+		return (result);
 }
